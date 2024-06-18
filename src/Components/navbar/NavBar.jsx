0c36b6797f43c2a7ff0b3cfb416cfa93d7fb8005
scourtepatte <srcourtepatte@gmail.com>
2024-06-17 20:33:21 -0600

    establish project structure

---
+++ src/Components/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+
+const NavBar = ()=>{
+
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>My Kingdom</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
---
+++ src/Views/Homepage.jsx
@@ -0,0 +1,12 @@
+
+
+const Homepage = ()=>{
+
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default Homepage;
