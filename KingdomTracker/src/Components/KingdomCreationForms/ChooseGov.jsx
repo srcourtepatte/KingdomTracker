6df5946fc8c0c5606bef5b13604dfe75edaa6f1b
srcourtepatte <srcourtepatte@gmail.com>
2024-07-29 21:00:20 -0600

    updated handling for free ability choices working on error handling for going over allowed limit for ability scores

--- KingdomTracker/src/Components/KingdomCreationForms/ChooseGov.jsx
+++ KingdomTracker/src/Components/KingdomCreationForms/ChooseGov.jsx
@@ -23,6 +23,10 @@ const ChooseGov = ()=>{
             
             const selection = document.getElementById(currentGovernment.name);
             selection.classList.remove("selected");
+            const currVal1 = sessionStorage.getItem(currentGovernment.boost1);
+            const currVal2 = sessionStorage.getItem(currentGovernment.boost2);
+            sessionStorage.setItem(req.boost1, parseInt(currVal1) - 1);
+            sessionStorage.setItem(req.boost2, parseInt(currVal2) - 1);
 
         }
 
@@ -30,12 +34,16 @@ const ChooseGov = ()=>{
         selection.classList.add("selected");
         setCurrentGovernment(req);
         sessionStorage.setItem("government", req.id);
+        const currVal1 = sessionStorage.getItem(req.boost1);
+        const currVal2 = sessionStorage.getItem(req.boost2); 
+        sessionStorage.setItem(req.boost1, parseInt(currVal1) + 1);
+        sessionStorage.setItem(req.boost2, parseInt(currVal2) + 1);
     };
 
     return (
         <div className="aspectGrid">
         {governments.map((government) =>(
-            <div className="aspectCard" id={government.government_name} onClick={(e)=>{setSelected({name: government.government_name, id: government.government_id})}}>
+            <div className="aspectCard" id={government.government_name} onClick={(e)=>{setSelected({name: government.government_name, id: government.government_id, boost1: government.ab_1, boost2: government.ab_2})}}>
                 <h1>{government.government_name}</h1>
                 <p>{government.government_description} </p>
                 <p>Ability Boosts: {government.ab_1}, {government.ab_2}, {government.ab_3}</p>
--- KingdomTracker/src/Components/KingdomCreationForms/FinalDetails.jsx
+++ KingdomTracker/src/Components/KingdomCreationForms/FinalDetails.jsx
@@ -4,6 +4,8 @@ const FinalDetails = ()=>{
 
     const [name, setName] = useState();
     const [level, setLevel] = useState(1);
+    const [chosen1, setChosen1] = useState("");
+    const [chosen2, setChosen2] = useState("");
 
     // sessionStorage.setItem("level", 1);
 
@@ -25,6 +27,53 @@ const FinalDetails = ()=>{
          
     };
 
+const setFirstBonus = (e)=>{
+    const bonusName = e.target.value;
+
+    if(chosen1 != "")
+    {
+        const currVal = sessionStorage.getItem(chosen1)
+        sessionStorage.setItem(chosen1, parseInt(currVal) - 1);
+    }
+
+    setChosen1(bonusName);
+    const currVal = sessionStorage.getItem(bonusName);
+    sessionStorage.setItem(bonusName, parseInt(currVal) + 1);
+    sessionStorage.setItem("bonus1", e.target.value);
+}
+
+const setSecondBonus = (e)=>{
+    const bonusName = e.target.value;
+
+    if(chosen1 != "")
+    {
+        const currVal = sessionStorage.getItem(chosen1)
+        sessionStorage.setItem(chosen2, parseInt(currVal) - 1);
+    }
+    console.log("here3");
+    setChosen2(bonusName);
+    const currVal = sessionStorage.getItem(bonusName);
+    sessionStorage.setItem(bonusName, parseInt(currVal) + 1);
+    sessionStorage.setItem("bonus2", bonusName);
+
+}
+
+const checkValid = (name)=>{
+    const currVal = sessionStorage.getItem(name);
+    console.log(name);
+    console.log(currVal);
+    if (parseInt(currVal) > 3) 
+    {
+        console.log("above3");
+        return "false";
+    } 
+    else
+    {
+        return "false";
+    }
+    
+}
+
     return (
         <div className="signInForm">
         <label>Kingdom Name: </label>
@@ -32,6 +81,44 @@ const FinalDetails = ()=>{
 
         <label >Kingdom Level: </label>
         <input type="number" min="1" max="20" defaultValue="0" name="kingdomLevel" onChange={handleChange}/>
+
+        <h4>Additional Ability Boosts (choose 2)</h4>
+
+        <div>
+            <label htmlFor="culture"  onClick={(e)=>{setFirstBonus(e)}}>
+            <input type="radio" name="addboost" id="culture" value="Culture" />
+            Culture</label>
+
+            <label htmlFor="economy" onClick={(e)=>{setFirstBonus(e)}} >
+            <input type="radio" name="addboost" id="economy" value="Economy" />
+            Economy</label>
+
+            <label htmlFor="Loyalty" onClick={(e)=>{setFirstBonus(e)}} >
+            <input type="radio" name="addboost" id="Loyalty" value="Loyalty" />
+            Loyalty</label>
+
+            <label htmlFor="stability" onClick={(e)=>{setFirstBonus(e)}} > 
+            <input type="radio" name="addboost" id="Stability" value="Stability" />
+            Stability</label>
+        </div>
+        
+        <div>
+            <label htmlFor="culture" onClick={(e)=>{setSecondBonus(e)}}>
+            <input type="radio" name="addboost2" id="Culture" value="Culture" />
+            Culture</label>
+            
+            <label htmlFor="economy" onClick={(e)=>{setSecondBonus(e)}}>
+            <input type="radio" name="addboost2" id="Economy" value="Economy" />
+            Economy</label>
+            
+            <label htmlFor="Loyalty" onClick={(e)=>{setSecondBonus(e)}}>
+            <input type="radio" name="addboost2" id="Loyalty" value="Loyalty" />
+            Loyalty</label>
+
+            <label htmlFor="stability" onClick={(e)=>{setSecondBonus(e)}}>
+            <input type="radio" name="addboost2" id="Stability" value="Stability" />
+            Stability</label>
+        </div>
 
     </div>
     );
