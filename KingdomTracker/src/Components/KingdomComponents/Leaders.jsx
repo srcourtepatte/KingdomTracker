a2cc2bf8d0e22adc303a9d0c187f187daab82620
srcourtepatte <srcourtepatte@gmail.com>
2024-08-12 08:46:52 -0600

    added investment checkbox for leaders created text input for holding leader names - set disabled while on sheet

--- KingdomTracker/src/Components/KingdomComponents/Leaders.jsx
+++ KingdomTracker/src/Components/KingdomComponents/Leaders.jsx
@@ -2,17 +2,31 @@ import '../../style/main.css';
 
 const Leaders = (data)=>{
 console.log(data.data);
-    const positions = [
-        "Ruler", "Counselor", "General",  "Emissary", "Magister", "Treasurer", "Viceroy", "Warden"
-    ]
+
         
+    const checkInvestment = (position)=>{
+
+        if (position.invested < 1){
+            return "";
+        } 
+        else{
+            return "true";
+        }
+            
+    }
 
     return (
         <div className='statForm'>
+                
+                <label className='label item1'> Inv</label>
+                <label className='label item2'> Role</label>
+                <label className='label item3'> Name</label>
+            
             {data.data.map((position) => (
                 <>
-                <label className='nameLabel item1'>{position.role_name}</label>
-                <label className='label item2'>{position.leader_name}</label>
+                <input type="checkbox" name="invested" id="invested" className='item1' checked={checkInvestment(position)} />
+                <label className='nameLabel item2'>{position.role_name}</label>
+                <input type="text" className='item3 leaderNames' value={position.leader_name} disabled="true"/>
                 </>   
             ))}
         </div>
