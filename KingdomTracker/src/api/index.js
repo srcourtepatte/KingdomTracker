e57381fef6d49c526611439a8bc8cdd564fb06c8
srcourtepatte <srcourtepatte@gmail.com>
2025-04-21 12:15:37 -0600

    update to try integrating vercel app

--- KingdomTracker/src/api/index.js
+++ KingdomTracker/src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: 'https://kingdomtracker.onrender.com/',
             headers: {
-            origin: "http://localhost:5173",
+            origin: "https://kingdom-tracker.vercel.app/",
         },
     
 });
