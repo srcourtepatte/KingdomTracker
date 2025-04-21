353841356facaf0d8e7584fe1a0fbeb839797b48
srcourtepatte <151782527+srcourtepatte@users.noreply.github.com>
2025-04-21 10:29:53 -0600

    Update connect.js

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -5,12 +5,14 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.PORT;
 
 const connection = mySql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
+    port: DB_PORT,
     insecureAuth: true
 
 });
